293d9ef76876678583ae67af7dc6a522d487940c
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2021-11-20 15:25:43 -0500

    added celldata struct

---
+++ Assets/Scripts/World/MeshGeneration/CellData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public struct CellData
+{
+	public Vector3 vertex1;
+	public Vector3 vertex2;
+	public Vector3 vertex3;
+	public int triangle1;
+	public int triangle2;
+	public int triangle3;
+}
