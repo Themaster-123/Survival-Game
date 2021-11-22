ec16306c644026586da1ad48fa36ffd7eacf3629
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2021-11-22 09:35:23 -0500

    added stencil class

---
+++ Assets/Scripts/World/Stencils/Stencil.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stencil
+{
+	public virtual void SetVoxel(float value, Vector3Int pos, World world)
+	{
+
+	}
+}
