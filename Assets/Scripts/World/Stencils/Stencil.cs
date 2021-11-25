3901317108dd722d938d469bdccb480ca3767221
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2021-11-25 02:29:45 -0500

    Made Stencils Serializable

--- Assets/Scripts/World/Stencils/Stencil.cs
+++ Assets/Scripts/World/Stencils/Stencil.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+[Serializable]
 public class Stencil
 {
 	public virtual void SetVoxel(Voxel voxel, Vector3Int pos, World world)
