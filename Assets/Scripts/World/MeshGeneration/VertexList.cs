222299215b76ab4480fbbe42d7f7ce817dea8896
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2021-11-28 13:34:58 -0500

    Changed VertexList to a struct

--- Assets/Scripts/World/MeshGeneration/VertexList.cs
+++ Assets/Scripts/World/MeshGeneration/VertexList.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VertexList : IEnumerable<Vector3>
+public struct VertexList : IEnumerable<Vector3>
 {
 	private Vector3 vertex0;
 	private Vector3 vertex1;
@@ -22,11 +22,53 @@ public class VertexList : IEnumerable<Vector3>
 	{
 		get
 		{
-			return GetVertexByIndex(index);
+			switch (index)
+			{
+				case 0: return vertex0;
+				case 1: return vertex1;
+				case 2: return vertex2;
+				case 3: return vertex3;
+				case 4: return vertex4;
+				case 5: return vertex5;
+				case 6: return vertex6;
+				case 7: return vertex7;
+				case 8: return vertex8;
+				case 9: return vertex9;
+				case 10: return vertex10;
+				case 11: return vertex11;
+				default: throw new ArgumentOutOfRangeException($"There are only 12 vertices! You tried to access the vertex at index {index}");
+			}
 		}
 		set
 		{
-			GetVertexByIndex(index) = value;
+			switch (index)
+			{
+				case 0: vertex0 = value;
+					break;
+				case 1: vertex1 = value;
+					break;
+				case 2: vertex2 = value;
+					break;
+				case 3: vertex3 = value;
+					break;
+				case 4: vertex4 = value;
+					break;
+				case 5: vertex5 = value;
+					break;
+				case 6: vertex6 = value;
+					break;
+				case 7: vertex7 = value;
+					break;
+				case 8: vertex8 = value;
+					break;
+				case 9: vertex9 = value;
+					break;
+				case 10: vertex10 = value;
+					break;
+				case 11: vertex11 = value;
+					break;
+				default: throw new ArgumentOutOfRangeException($"There are only 12 vertices! You tried to access the vertex at index {index}");
+			}
 		}
 	}
 
@@ -41,25 +83,5 @@ public class VertexList : IEnumerable<Vector3>
 	IEnumerator IEnumerable.GetEnumerator()
 	{
 		return this.GetEnumerator();
-	}
-
-	private ref Vector3 GetVertexByIndex(int index)
-	{
-		switch (index)
-		{
-			case 0: return ref vertex0;
-			case 1: return ref vertex1;
-			case 2: return ref vertex2;
-			case 3: return ref vertex3;
-			case 4: return ref vertex4;
-			case 5: return ref vertex5;
-			case 6: return ref vertex6;
-			case 7: return ref vertex7;
-			case 8: return ref vertex8;
-			case 9: return ref vertex9;
-			case 10: return ref vertex10;
-			case 11: return ref vertex11;
-			default: throw new ArgumentOutOfRangeException($"There are only 12 vertices! You tried to access the vertex at index {index}");
-		}
 	}
 }
