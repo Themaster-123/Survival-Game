701b383a2f5277ac7c3bcbab3d274a0a0471bd3a
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2021-11-29 01:16:56 -0500

    fixed NativeCounter not working for burst

--- Assets/Scripts/Job Types/NativeCounter.cs
+++ Assets/Scripts/Job Types/NativeCounter.cs
@@ -7,24 +7,17 @@ using System.Runtime.InteropServices;
 using System.Threading;
 
 [StructLayout(LayoutKind.Sequential)]
-[NativeContainer]
 unsafe public struct NativeCounter : IDisposable
 {
 	public int Count
 	{
 		get
 		{
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-			AtomicSafetyHandle.CheckReadAndThrow(m_Safety);
-#endif
 			return *m_Counter;
 		}
 
 		set
 		{
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-			AtomicSafetyHandle.CheckWriteAndThrow(m_Safety);
-#endif
 			*m_Counter = value;
 		}
 	}
@@ -36,13 +29,6 @@ unsafe public struct NativeCounter : IDisposable
 	[NativeDisableUnsafePtrRestriction]
 	private int* m_Counter;
 
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-	private AtomicSafetyHandle m_Safety;
-
-	[NativeSetClassTypeToNullOnSchedule]
-	private DisposeSentinel m_DisposeSentinel;
-#endif
-
 	Allocator m_AllocatorLabel;
 
 	public NativeCounter(Allocator label)
@@ -51,26 +37,16 @@ unsafe public struct NativeCounter : IDisposable
 
 		m_Counter = (int*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<int>(), 4, label);
 
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-		DisposeSentinel.Create(out m_Safety, out m_DisposeSentinel, 0, m_AllocatorLabel);
-#endif
 		Count = 0;
 	}
 
 	public int Increment()
 	{
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-		AtomicSafetyHandle.CheckWriteAndThrow(m_Safety);
-#endif
 		return Interlocked.Increment(ref *m_Counter) - 1;
 	}
 
 	public void Dispose()
 	{
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-		DisposeSentinel.Dispose(ref m_Safety, ref m_DisposeSentinel);
-#endif
-
 		UnsafeUtility.Free(m_Counter, m_AllocatorLabel);
 		m_Counter = null;
 	}
