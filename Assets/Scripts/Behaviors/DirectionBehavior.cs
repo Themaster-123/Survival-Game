a0228f1083f3d326ac13906e8970ca913c8ee3d7
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2021-12-17 12:59:00 -0500

    Changed DirectionBehavior parent to Behavior class

--- Assets/Scripts/Behaviors/DirectionBehavior.cs
+++ Assets/Scripts/Behaviors/DirectionBehavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DirectionBehavior : MonoBehaviour
+public class DirectionBehavior : Behavior
 {
     public bool rotateOnXAxis = true;
     public bool rotateOnYAxis = false;
