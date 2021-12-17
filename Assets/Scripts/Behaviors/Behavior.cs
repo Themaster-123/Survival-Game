8743d62a3b7415a5816f49d3fdfd225d4951cd74
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2021-12-17 12:57:26 -0500

    Added Behavior class

---
+++ Assets/Scripts/Behaviors/Behavior.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behavior : MonoBehaviour
+{
+	protected virtual void Awake()
+	{
+		GetComponents();
+	}
+
+	protected virtual void GetComponents()
+	{
+	}
+}
