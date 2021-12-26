bfd3b9e9fae17e3abdc84f10459a4b128fb827c6
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2021-12-26 15:03:41 -0500

    Removed entity dependencies

--- Assets/Scripts/Behaviors/BoundsBehavior.cs
+++ Assets/Scripts/Behaviors/BoundsBehavior.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Entity))]
 [RequireComponent(typeof(Rigidbody))]
 public class BoundsBehavior : Behavior
 {
--- Assets/Scripts/Behaviors/ModifierBehavior.cs
+++ Assets/Scripts/Behaviors/ModifierBehavior.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Entity))]
 public class ModifierBehavior : Behavior
 {
     public Stencil stencil;
