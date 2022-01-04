9af35b02685666a46b5d6abb9cd19511d8b66e48
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-01-04 15:42:59 -0500

    Added CalculateFCost method to PathNode class

--- Assets/Scripts/Pathfinding/PathNode.cs
+++ Assets/Scripts/Pathfinding/PathNode.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class PathNode
 {
+	public Vector2Int gridPosition;
+
 	public int gCost;
 	public int hCost;
 	public int fCost;
+
 	public PathNode prevNode;
+	public bool walkable = true;
 
 	protected Grid<PathNode> grid;
-	protected Vector2Int gridPosition;
 
 	public PathNode(in Grid<PathNode> grid, Vector2Int gridPosition)
 	{
 		this.grid = grid;
 		this.gridPosition = gridPosition;
+	}
+
+	public void CalculateFCost()
+	{
+		fCost = gCost + hCost;
 	}
 
 	public override string ToString()
