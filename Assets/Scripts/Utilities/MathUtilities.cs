149cf4fcdc082f3a6c6706ce7da0a762341268a9
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-01-08 16:00:41 -0500

    Added Abs Method in MathUtilities class

--- Assets/Scripts/Utilities/MathUtilities.cs
+++ Assets/Scripts/Utilities/MathUtilities.cs
@@ -15,4 +15,24 @@ public static class MathUtilities
 		int r = a % b;
 		return r < 0 ? r + b : r;
 	}
+
+	public static Vector3 Abs(Vector3 vector)
+	{
+		return new Vector3(Mathf.Abs(vector.x), Mathf.Abs(vector.y), Mathf.Abs(vector.z));
+	}
+
+	public static Vector2 Abs(Vector2 vector)
+	{
+		return new Vector2(Mathf.Abs(vector.x), Mathf.Abs(vector.y));
+	}
+
+	public static Vector3Int Abs(Vector3Int vector)
+	{
+		return new Vector3Int(Mathf.Abs(vector.x), Mathf.Abs(vector.y), Mathf.Abs(vector.z));
+	}
+
+	public static Vector2Int Abs(Vector2Int vector)
+	{
+		return new Vector2Int(Mathf.Abs(vector.x), Mathf.Abs(vector.y));
+	}
 }
