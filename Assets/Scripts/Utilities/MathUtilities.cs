1a362d25356df9614cd0aacc3cfbd7d19c3fb996
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-01-19 13:43:41 -0500

    Added FindDerivative and FindGradientVector methods in MathUtilities

--- Assets/Scripts/Utilities/MathUtilities.cs
+++ Assets/Scripts/Utilities/MathUtilities.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public static class MathUtilities
 {
+	public static float limit = 0.000001f;
+
 	public static float Mod(float a, float b)
 	{
 		float r = a % b;
@@ -34,5 +37,19 @@ public static class MathUtilities
 	public static Vector2Int Abs(Vector2Int vector)
 	{
 		return new Vector2Int(Mathf.Abs(vector.x), Mathf.Abs(vector.y));
+	}
+
+	public static float FindDerivative(int axis, Func<int, float> function)
+	{
+		return (function(axis + 1) - function(axis - 1)) / 2;
+	}
+
+	public static Vector3 FindGradientVector(Vector3Int pos, Func<Vector3Int, float> function)
+	{
+		float divX = FindDerivative(pos.x, (int x) => function(new Vector3Int(x, pos.y, pos.z)));
+		float divY = FindDerivative(pos.y, (int y) => function(new Vector3Int(pos.x, y, pos.z)));
+		float divZ = FindDerivative(pos.z, (int z) => function(new Vector3Int(pos.x, pos.y, z)));
+
+		return new Vector3(divX, divY, divZ);
 	}
 }
