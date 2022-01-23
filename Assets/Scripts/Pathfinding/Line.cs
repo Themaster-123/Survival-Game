8b49dae9e5caeaa26267e3c579d6d0d7c0ca23fb
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-01-23 02:38:01 -0500

    Added Line struct.

---
+++ Assets/Scripts/Pathfinding/Line.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Line
+{
+	public readonly static float VERTICAL_LINE_GRADIENT = 1e10f;
+
+	private float gradient;
+	private float yIntercept;
+	private float gradientPerpendicular;
+
+	private Vector2 pointOnLine1;
+	private Vector2 pointOnLine2;
+	private bool approachSide;
+
+	public Line(Vector2 pointOnLine, Vector2 pointPerpendicularToLine)
+	{
+		Vector2 dPoint = pointOnLine - pointPerpendicularToLine;
+		
+		if (dPoint.x == 0)
+		{
+			gradientPerpendicular = VERTICAL_LINE_GRADIENT;
+		}
+		else
+		{
+			gradientPerpendicular = dPoint.y / dPoint.x;
+		}
+		
+		if (gradientPerpendicular == 0)
+		{
+			gradient = VERTICAL_LINE_GRADIENT;
+		}
+		else
+		{
+			gradient = -1 / gradientPerpendicular;
+		}
+
+		yIntercept = pointOnLine.y - gradient * pointOnLine.x;
+
+		pointOnLine1 = pointOnLine;
+		pointOnLine2 = pointOnLine1 + new Vector2(1, gradient);
+
+		approachSide = false;
+		approachSide = GetSide(pointPerpendicularToLine);
+	}
+
+	public bool GetSide(Vector2 point)
+	{
+		return (point.x - pointOnLine1.x) * (pointOnLine2.y - pointOnLine1.y) - (point.y - pointOnLine1.y) * (pointOnLine2.x - pointOnLine1.x) < 0;
+	}
+
+	public bool HasCrossedLine(Vector2 point)
+	{
+		return GetSide(point) != approachSide;
+	}
+}
