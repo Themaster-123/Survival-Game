bbe96ab5a447219c6f4d971fddea742b28d6128b
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-01-26 13:09:45 -0500

    Made Rabbit class use PathFollowerBehavior

--- Assets/Scripts/Entities/Rabbit.cs
+++ Assets/Scripts/Entities/Rabbit.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 [RequireComponent(typeof(MovementBehavior))]
 [RequireComponent(typeof(DirectionBehavior))]
+[RequireComponent(typeof(PathFollowerBehavior))]
 public class Rabbit : Entity
 {
+	public Transform target; 
 	protected MovementBehavior movementBehavior;
 	protected DirectionBehavior directionBehavior;
+	protected PathFollowerBehavior pathFollowerBehavior;
+
+	protected override void Start()
+	{
+		base.Start();
+	}
+
+	protected override void Update()
+	{
+		base.Update();
+	}
 
 	protected override void MoveEntity()
 	{
 		base.MoveEntity();
-		movementBehavior.Move(directionBehavior.HorizontalDirection());
+		pathFollowerBehavior.FindPath(target.position);
+
+		//movementBehavior.Move(directionBehavior.HorizontalDirection());
 	}
 
 	protected override void GetComponents()
@@ -20,5 +35,6 @@ public class Rabbit : Entity
 		base.GetComponents();
 		movementBehavior = GetComponent<MovementBehavior>();
 		directionBehavior = GetComponent<DirectionBehavior>();
+		pathFollowerBehavior = GetComponent<PathFollowerBehavior>();
 	}
 }
