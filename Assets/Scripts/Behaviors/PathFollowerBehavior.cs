80ae3548ac2a199bc12de90aa99b97c66ee5da42
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-01-26 13:55:01 -0500

    Made PathFollowerBehavior slow down when it gets close enough

--- Assets/Scripts/Behaviors/PathFollowerBehavior.cs
+++ Assets/Scripts/Behaviors/PathFollowerBehavior.cs
@@ -9,7 +9,8 @@ public class PathFollowerBehavior : Behavior
 	public float maxPathDistance = 1f;
 	[Range(0, 100)]
 	public float steeringStrength = 25f;
-	public float slowDownDistance = 1f;
+	public float maxSlowDownDistance = 1f;
+	public float minSlowDownDistance = .5f;
 
 	public Vector3[] CurrentPath { get; protected set; }
 	protected MovementBehavior movementBehavior;
@@ -39,14 +40,25 @@ public class PathFollowerBehavior : Behavior
 			currentPathIndex++;
 		}
 
-		Vector2 direction = MathUtilities.Flatten(CurrentPath[currentPathIndex]) - MathUtilities.Flatten(transform.position);
+		Vector2 offset = MathUtilities.Flatten(CurrentPath[currentPathIndex]) - MathUtilities.Flatten(transform.position);
+		Vector2 direction = offset.normalized;
 
-		Vector3 desiredVelocity = MathUtilities.UnFlatten(direction.normalized);
+		Vector3 desiredVelocity = MathUtilities.UnFlatten(direction);
 		Vector3 movementVelocity = movementBehavior.MovementVelocity;
 		Vector3 steering = desiredVelocity - movementVelocity;
 		steering *= steeringStrength * 0.01f;
+		float speed = 1;
 
-		movementBehavior.Move((movementVelocity + steering) * Time.fixedDeltaTime);
+		if (currentPathIndex == CurrentPath.Length - 1)
+		{
+			if (offset.sqrMagnitude <= maxSlowDownDistance * maxSlowDownDistance)
+			{
+				float clampedDistance = Mathf.Clamp(direction.magnitude, minSlowDownDistance, maxSlowDownDistance);
+				speed = MathUtilities.MapToRange(clampedDistance, minSlowDownDistance, maxSlowDownDistance, 0f, 1f);
+			}
+		}
+
+		movementBehavior.Move((movementVelocity + steering) * speed * Time.fixedDeltaTime);
 	}
 
 	protected override void GetComponents()
