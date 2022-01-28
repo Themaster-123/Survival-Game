423a135f46f511482a40db1eb683f7e30e5be857
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-01-27 20:21:22 -0500

    Added WandererBehavior class

---
+++ Assets/Scripts/Behaviors/WandererBehavior.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PathFollowerBehavior))]
+public class WandererBehavior : Behavior
+{
+
+	public float maxWanderTime = 6f;
+	public float minWanderTime = 3f;
+	public float maxWanderRange = 5f;
+	public float minWanderRange = 2f;
+	protected PathFollowerBehavior pathFollowerBehavior;
+	protected float wanderTime = 0f;
+
+	public virtual void Wander()
+	{
+		Vector3 wanderPoint = MathUtilities.GenerateRandomPointOnAnnulus(transform.position, minWanderRange, maxWanderRange);
+		pathFollowerBehavior.FindPath(wanderPoint);
+	}
+
+	protected virtual void Start()
+	{
+		SetWanderTime();
+	}
+
+	protected virtual void FixedUpdate()
+	{
+		TryWander();
+	}
+
+	protected override void GetComponents()
+	{
+		base.GetComponents();
+		pathFollowerBehavior = GetComponent<PathFollowerBehavior>();
+	}
+
+	protected virtual void TryWander()
+	{
+		if (Time.fixedTime >= wanderTime)
+		{
+			SetWanderTime();
+			Wander(); 
+		}
+	}
+
+	protected virtual float SetWanderTime()
+	{
+		wanderTime = Time.fixedTime + Random.Range(minWanderTime, maxWanderTime);
+		return wanderTime;
+	}
+}
