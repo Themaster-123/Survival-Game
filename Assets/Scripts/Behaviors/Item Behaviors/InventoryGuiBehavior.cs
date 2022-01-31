4a7b20206bf8cefa5418579c1bddb2d7b5ebb6af
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-01-31 16:07:36 -0500

    Made InventoryGuiBehavior show item at cursor

--- Assets/Scripts/Behaviors/Item Behaviors/InventoryGuiBehavior.cs
+++ Assets/Scripts/Behaviors/Item Behaviors/InventoryGuiBehavior.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(InventoryBehavior))]
+[RequireComponent(typeof(InputBehavior))]
 public class InventoryGuiBehavior : Behavior
 {
 	public bool Opened 
@@ -15,30 +16,33 @@ public class InventoryGuiBehavior : Behavior
 	}
 
 	public Transform inventoryUI;
+	public RectTransform cursorSlot;
 	public Item cursorItem;
 	protected InventoryBehavior inventoryBehavior;
+	protected InputBehavior inputBehavior;
 	protected Button[,] slots;
 	protected Image[,] slotSprites;
+	protected Image cursorSlotSprite;
 
-	public void Open()
+	public virtual void Open()
 	{
 		inventoryUI.gameObject.SetActive(true);
 		GameUtils.UnlockMouse();
 	}
 
-	public void Close()
+	public virtual void Close()
 	{
 		inventoryUI.gameObject.SetActive(false);
 		GameUtils.LockMouse();
 	}
 
-	public void Toggle()
+	public virtual void Toggle()
 	{
 		if (Opened) Close();
 		else Open();
 	}
 
-	public void OnClickSlot(Vector2Int slot)
+	public virtual void OnClickSlot(Vector2Int slot)
 	{
 		Item oldItem = inventoryBehavior.inventory[slot];
 		inventoryBehavior.inventory[slot] = cursorItem;
@@ -61,6 +65,7 @@ public class InventoryGuiBehavior : Behavior
 	{
 		base.GetComponents();
 		inventoryBehavior = GetComponent<InventoryBehavior>();
+		inputBehavior = GetComponent<InputBehavior>();
 	}
 
 	protected virtual void UpdateGui()
@@ -71,23 +76,27 @@ public class InventoryGuiBehavior : Behavior
 		{
 			for (int y = 0; y < inventoryBehavior.size.y; y++)
 			{
-				slotSprites[x, y].sprite = inventoryBehavior.inventory[x, y].sprite;
-				slotSprites[x, y].color = slotSprites[x, y].sprite != null ? Color.white : new Color(0, 0, 0, 0);
+				SetImageToItem(slotSprites[x, y], inventoryBehavior.inventory[x, y]);
 			}
 		}
+
+		SetImageToItem(cursorSlotSprite, cursorItem);
+		cursorSlot.position = inputBehavior.MousePosition;
+		GameUtils.ShowCursor(cursorItem == ItemType.Air);
 	}
 
 	protected virtual void GetSlots()
 	{
 		slots = new Button[inventoryBehavior.size.x, inventoryBehavior.size.y];
 		slotSprites = new Image[inventoryBehavior.size.x, inventoryBehavior.size.y];
+		cursorSlotSprite = cursorSlot.GetComponent<Image>();
 
 		for (int i = 0; i < inventoryBehavior.size.x * inventoryBehavior.size.y; i++)
 		{
 			int x = i % inventoryBehavior.size.x;
 			int y = i / inventoryBehavior.size.x;
 
-			Transform slot = inventoryUI.GetChild(i);
+			Transform slot = inventoryUI.GetChild(0).GetChild(i);
 			slots[x, y] = slot.GetComponent<Button>();
 			slotSprites[x, y] = slot.GetChild(0).GetComponent<Image>();
 			slots[x, y].onClick.AddListener(() => { OnClickSlot(new Vector2Int(x, y)); });
@@ -97,5 +106,11 @@ public class InventoryGuiBehavior : Behavior
 	protected virtual void InitializeCursor()
 	{
 		cursorItem = ItemDatabase.GetItem(ItemType.Air);
+	}
+
+	protected void SetImageToItem(Image image, Item item)
+	{
+		image.sprite = item.sprite;
+		image.color = image.sprite != null ? Color.white : new Color(0, 0, 0, 0);
 	}
 }
