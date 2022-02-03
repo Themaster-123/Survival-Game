eef409ad35c3d2c33ceccae551abf5373d2ae886
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-02-03 01:58:01 -0500

    Added EqualsIgnoreStackSize and Equals and GetHashCode methods

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 
 public class Item : ICloneable, IEquatable<Item>, IEquatable<ItemType>
 {
+	public int StackSize
+	{
+		get
+		{
+			return stackSize;
+		}
+
+		set
+		{
+			stackSize = Math.Max(Math.Min(value, MaxStackSize), 1);
+		}
+	}
+
+	public ItemType Type { get; protected set; }
+	public int MaxStackSize { get; protected set; }
+
 	public string name;
 	public string description;
 	public Sprite sprite;
-	public ItemType Type { get; protected set; }
+
+	protected int stackSize;
 
 	public Item(ItemSettings itemSettings)
 	{
@@ -16,6 +33,8 @@ public class Item : ICloneable, IEquatable<Item>, IEquatable<ItemType>
 		description = itemSettings.description;
 		sprite = itemSettings.sprite;
 		Type = itemSettings.itemType;
+		MaxStackSize = itemSettings.maxStackSize;
+		StackSize = 1;
 	}
 
 	public object Clone()
@@ -25,12 +44,42 @@ public class Item : ICloneable, IEquatable<Item>, IEquatable<ItemType>
 
 	public bool Equals(Item other)
 	{
+		return EqualsIgnoreStackSize(other) && StackSize == other.StackSize;
+	}
+
+	public bool EqualsIgnoreStackSize(Item other)
+	{
 		return Type == other.Type && name == other.name && description == other.description;
 	}
 
 	public bool Equals(ItemType other)
 	{
 		return Type == other;
+	}
+
+	public override bool Equals(object other)
+	{
+		if (typeof(Item).IsInstanceOfType(other))
+		{
+			return Equals((Item)other);
+		}
+		else if (typeof(ItemType).IsInstanceOfType(other))
+		{
+			return Equals((ItemType)other);
+		}
+
+		return false;
+	}
+
+	public override int GetHashCode()
+	{
+		int hashCode = -1294763954;
+		hashCode = hashCode * -1521134295 + StackSize.GetHashCode();
+		hashCode = hashCode * -1521134295 + Type.GetHashCode();
+		hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(name);
+		hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(description);
+		hashCode = hashCode * -1521134295 + EqualityComparer<Sprite>.Default.GetHashCode(sprite);
+		return hashCode;
 	}
 
 	public static bool operator ==(Item a, Item b)
