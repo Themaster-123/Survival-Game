fe6e65e33dd0598cb6e932e38eb6cbe8bb1d9812
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-02-04 21:09:29 -0500

    Add added CreateWorldItem method in GameUtils class

--- Assets/Scripts/Utilities/GameUtils.cs
+++ Assets/Scripts/Utilities/GameUtils.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class GameUtils
+public class GameUtils : MonoBehaviour
 {
+    public GameObject itemBehaviorPrefab;
+    protected static GameUtils instance;
+
     public static void LockMouse()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -18,5 +21,29 @@ public static class GameUtils
     public static void ShowCursor(bool show)
 	{
         Cursor.visible = show;
+	}
+
+    public static ItemBehavior CreateWorldItem(Item item)
+	{
+        return instance.InstanceCreateWorldItem(item);
+	}
+
+	protected void Awake()
+	{
+		if (instance == null)
+		{
+            instance = this;
+		}
+	}
+
+	protected ItemBehavior InstanceCreateWorldItem(Item item)
+	{
+        item = (Item)item.Clone();
+        item.StackSize = 1;
+
+        GameObject gameObject = Instantiate(itemBehaviorPrefab, Vector3.zero, Quaternion.identity);
+        ItemBehavior itemBehavior = gameObject.GetComponent<ItemBehavior>();
+        itemBehavior.item = item;
+        return itemBehavior;
 	}
 }
